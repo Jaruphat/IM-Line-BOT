febe7d3aac29aa7eef8cfaa35af4d98c7c1070e8
Jaruphat <44111855+Jaruphat@users.noreply.github.com>
2018-10-13 18:08:48 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'sWFWYEZmlNJIQX+IZrkI3eKsX9fhmnc2mnfEZF2jaoO+KSu68fl+1rYsoic+E2ZWChVgNHvmqzXA3Ioe1VcpoqV0LMHQgfYRXv3HI8A27DRmwSGgtYQVBW9T66BCZSjZCKpIT3HbITjvF8836OGSdgdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U5ace03d6c9ff4a67a5006b48fc5e2491';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
